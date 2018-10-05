b15643515d48a914af04baba62aabdcaa0d1efcd
Morimoto <username0777@hotmail.co.jp>
2018-10-05 09:29:41 +0900

    プロジェクト ファイルを追加します。

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("sbi_traded_to_csv")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("sbi_traded_to_csv")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("eb1fafa8-82bf-48d4-a2a5-bfff4e4c47e7")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
